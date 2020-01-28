c498e891dc7431ea2c31467fab497380ced89ec5
Mostafa Mahmoud <mostafamahmoud24@gmail.com>
2020-01-28 19:12:10 +0200

    about section is done

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,12 +1,36 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, HostListener, Inject } from "@angular/core";
+import {
+  trigger,
+  state,
+  transition,
+  style,
+  animate
+} from "@angular/animations";
+import { DOCUMENT } from "@angular/common";
 
 @Component({
   selector: "about",
   templateUrl: "./about.component.html",
-  styleUrls: ["./about.component.css"]
+  styleUrls: ["./about.component.css"],
+  animations: [
+    trigger("fade", [
+      state("void", style({})),
+      transition(":enter", [animate(300)]),
+      transition(":leave", [animate(500)])
+    ])
+  ]
 })
 export class AboutComponent implements OnInit {
-  constructor() {}
+  constructor(@Inject(DOCUMENT) document) {}
 
   ngOnInit() {}
+
+  @HostListener("window:scroll", ["$event"])
+  onWindowScroll(e) {
+    if (window.pageYOffset > 180) {
+      let element = document.getElementById("about-container");
+      element.classList.add("scroll-about-container");
+      element.classList.remove("about-container");
+    }
+  }
 }
