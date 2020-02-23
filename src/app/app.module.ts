89c03090412879f56e03314029b99f72daebb7fa
Mostafa Mahmoud <mostafamahmoud24@gmail.com>
2020-02-23 21:10:34 +0200

    deleted all redundant assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { NavbarComponent } from "./navbar/navbar.component";
 import { NgxPageScrollModule } from "ngx-page-scroll";
 import { AboutComponent } from "./about/about.component";
 import { Navbar1Component } from "./navbar1/navbar1.component";
@@ -13,13 +12,12 @@ import { PassionComponent } from "./passion/passion.component";
 import { ExperienceComponent } from "./experience/experience.component";
 import { WorkComponent } from "./work/work.component";
 import { MDBBootstrapModule } from "angular-bootstrap-md";
-import { ContactComponent } from './contact/contact.component';
-import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from "./contact/contact.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
     AboutComponent,
     Navbar1Component,
     HeroComponent,
